7cd38bda1bf242901da6c4ce92e4839c325a7709
Valik Shlandyk <VShlandyk@gomel.iba.by>
2021-01-25 00:08:01 +0300

    Understanding the Basic - task2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Header from './Header';
 import Card from './Card';
+import { useState } from 'react';
 
 function App() {
+  const [checked, setChecked] = useState(false);
+  const changeStyleHandler = () => {
+    setChecked(!checked);
+  }
+  const setClass = checked ? 'card-new-style' : 'card';
   return (
     <div className="App">
       <Header />
-      <Card caption='Caption' text='Text...'/>
+      <Card 
+        caption="Caption" 
+        text="Text..." 
+        onChange={changeStyleHandler} 
+        class_Name={setClass}
+      />
     </div>
   );
 }
--- src/Card/index.js
+++ src/Card/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './style.css';
 
-const card = ({caption, text}) => {
+const card = ({caption, text, onChange, class_Name}) => {
   return (
-    <div className='card'>
-        <div className='card-header'>{caption}</div>
+    <div className={class_Name}>
+        <div className='card-header'>
+          <div className='card-header-text'>{caption}</div>
+          <input type='checkbox' onChange={onChange} />
+        </div>
         <p className='card-text'>{text}</p>
     </div>
   )
