78f30e366f9dcedd12e73ca0a6de8e2ca7d424f2
Valik Shlandyk <VShlandyk@gomel.iba.by>
2021-01-28 01:46:40 +0300

    Understanding the Basic - task3

--- src/App.js
+++ src/App.js
@@ -3,18 +3,84 @@ import Card from './Card';
 import { useState } from 'react';
 
 function App() {
-  const [checked, setChecked] = useState(false);
+  const [cardState, setCardState] = useState({
+    checked: false,
+    isEditable: false
+  });
+
+  const [cardText, setCardText] = useState({
+    headerText: 'Caption',
+    tempHeader: 'Caption',
+    bodyText: 'text...',
+    tempBody: 'text...'
+  });
+
   const changeStyleHandler = () => {
-    setChecked(!checked);
-  }
+    setCardState({
+      ...cardState,
+      checked: !cardState.checked,
+    });
+  };
+
+  const openEditModeHandler = () => {
+    setCardState({
+      checked: false,
+      isEditable: !cardState.isEditable
+    });
+  };
+
+  const headerTextChangeHandler = e => {
+    setCardText({
+      ...cardText,
+      headerText: e.target.value
+    });
+  };
+
+  const bodyTextChangeHandler = e => {
+    setCardText({
+      ...cardText,
+      bodyText: e.target.value
+    });
+  };
+
+  const saveChangesHandler = () => {
+    setCardState({
+      ...cardState,
+      isEditable: !cardState.isEditable
+    });
+    setCardText({
+      ...cardText,
+      tempHeader: cardText.headerText,
+      tempBody: cardText.bodyText
+    })
+  };
+
+  const cancelChangesHandler = () => {
+    setCardState({
+      ...cardState,
+      isEditable: !cardState.isEditable
+    });
+    setCardText({
+      ...cardText,
+      headerText: cardText.tempHeader,
+      bodyText: cardText.tempBody
+    });
+  };
+
   return (
     <div className="App">
       <Header />
-      <Card 
-        caption="Caption" 
-        text="Text..." 
+      <Card
+        bodyText={cardText.bodyText}
         onChange={changeStyleHandler}
-        checked={checked}
+        checked={cardState.checked}
+        openEditMode={openEditModeHandler}
+        isEditable={cardState.isEditable}
+        onSaveChanges={saveChangesHandler}
+        onCancelChanges={cancelChangesHandler}
+        headerTextChangeHandler={headerTextChangeHandler}
+        headerText={cardText.headerText}
+        bodyTextChangeHandler={bodyTextChangeHandler}
       />
     </div>
   );
--- src/Card/index.js
+++ src/Card/index.js
@@ -1,14 +1,48 @@
 import React from 'react';
 import './style.css';
+import { BiEdit } from 'react-icons/bi';
+import { AiOutlineSave } from 'react-icons/ai';
+import { ImCancelCircle } from 'react-icons/im'
 
-const card = ({caption, text, onChange, checked}) => {
-  return (
-    <div className={checked ? 'card-new-style' : 'card'}>
-        <div className='card-header'>
-          <div className='card-header-text'>{caption}</div>
-          <input type='checkbox' onChange={onChange} />
+const card = props => {
+  const renderEditMode = () => {
+    return (
+      <div className='header-edit-mode-buttons' >
+          <AiOutlineSave className='save-button' onClick={props.onSaveChanges}/>
+          <ImCancelCircle className='cancel-button' onClick={props.onCancelChanges}/>
+       </div>
+      )
+  };
+
+  const renderReadMode = () => {
+    return (
+        <div className='header-default-buttons'>
+          <BiEdit className='edit-button' onClick={props.openEditMode}/>
+          <input type='checkbox' onChange={props.onChange} />
         </div>
-        <p className='card-text'>{text}</p>
+      )
+  };
+
+  return (
+    <div className={props.checked ? 'card active-status' : 'card'}>
+        <div className={props.checked ? 'card-header active' : 'card-header'}>
+          <input type="text"
+            className='card-header-text' 
+            readOnly={!props.isEditable}
+            value={props.headerText}
+            onChange={props.headerTextChangeHandler}
+          />
+          <div className='header-buttons' >
+            {props.isEditable ? renderEditMode() : renderReadMode()}
+          </div>
+        </div>
+        <textarea 
+          className={props.checked ? 'card-body active' : 'card-body'}
+          value={props.bodyText}
+          readOnly={!props.isEditable}
+          onChange={props.bodyTextChangeHandler}
+        >
+        </textarea>
     </div>
   )
 }
