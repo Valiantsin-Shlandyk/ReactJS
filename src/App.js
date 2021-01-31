aa4c3b6be41b5cb3f7da74dc0fd420ebab56eed8
Valik Shlandyk <VShlandyk@gomel.iba.by>
2021-01-31 20:41:52 +0300

    Understanding the Basic - task3 - fixed

--- src/App.js
+++ src/App.js
@@ -1,87 +1,11 @@
 import Header from './Header';
 import Card from './Card';
-import { useState } from 'react';
 
 function App() {
-  const [cardState, setCardState] = useState({
-    checked: false,
-    isEditable: false
-  });
-
-  const [cardText, setCardText] = useState({
-    headerText: 'Caption',
-    tempHeader: 'Caption',
-    bodyText: 'text...',
-    tempBody: 'text...'
-  });
-
-  const changeStyleHandler = () => {
-    setCardState({
-      ...cardState,
-      checked: !cardState.checked,
-    });
-  };
-
-  const openEditModeHandler = () => {
-    setCardState({
-      checked: false,
-      isEditable: !cardState.isEditable
-    });
-  };
-
-  const headerTextChangeHandler = e => {
-    setCardText({
-      ...cardText,
-      headerText: e.target.value
-    });
-  };
-
-  const bodyTextChangeHandler = e => {
-    setCardText({
-      ...cardText,
-      bodyText: e.target.value
-    });
-  };
-
-  const saveChangesHandler = () => {
-    setCardState({
-      ...cardState,
-      isEditable: !cardState.isEditable
-    });
-    setCardText({
-      ...cardText,
-      tempHeader: cardText.headerText,
-      tempBody: cardText.bodyText
-    })
-  };
-
-  const cancelChangesHandler = () => {
-    setCardState({
-      ...cardState,
-      isEditable: !cardState.isEditable
-    });
-    setCardText({
-      ...cardText,
-      headerText: cardText.tempHeader,
-      bodyText: cardText.tempBody
-    });
-  };
-
   return (
     <div className="App">
       <Header />
-      <Card
-        bodyText={cardText.bodyText}
-        onChange={changeStyleHandler}
-        checked={cardState.checked}
-        openEditMode={openEditModeHandler}
-        isEditable={cardState.isEditable}
-        onSaveChanges={saveChangesHandler}
-        onCancelChanges={cancelChangesHandler}
-        headerTextChangeHandler={headerTextChangeHandler}
-        headerText={cardText.headerText}
-        bodyTextChangeHandler={bodyTextChangeHandler}
-      />
+      <Card headerData='Caption' bodyData='Some interesting text'/>
     </div>
   );
 }
--- src/Card/index.js
+++ src/Card/index.js
@@ -1,15 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import { BiEdit } from 'react-icons/bi';
 import { AiOutlineSave } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im'
 
-const card = props => {
+const Card = props => {
+  const [cardState, setCardState] = useState({
+    checked: false,
+    isEditable: false,
+    data: {
+      headerData: props.headerData,
+      bodyData: props.bodyData
+    },
+    tempData: {
+      headerData: props.headerData,
+      bodyData: props.bodyData
+    }
+  });
+
+  const changeStyleHandler = () => {
+    setCardState({
+      ...cardState,
+      checked: !cardState.checked
+    })
+  };
+
+  const openEditModeHandler = () => {
+    setCardState({
+      ...cardState,
+      isEditable: !cardState.isEditable,
+      checked: false
+    })
+  };
+
+  const cardDataChangeHandler = (event, prop) => {
+    setCardState({
+      ...cardState,
+      tempData: {
+        ...cardState.tempData,
+        [prop]: event.target.value
+      }
+    })
+  };
+
+  const saveChangesHandler = () => {
+    setCardState({
+      ...cardState,
+      isEditable: false,
+      data: {
+        ...cardState.tempData
+      }
+    })
+  };
+
+  const cancelChangesHandler = () => {
+    setCardState({
+      ...cardState,
+      isEditable: false,
+      tempData: {
+        ...cardState.data
+      }
+    })
+  };
+
   const renderEditMode = () => {
     return (
       <div className='header-edit-mode-buttons' >
-          <AiOutlineSave className='save-button' onClick={props.onSaveChanges}/>
-          <ImCancelCircle className='cancel-button' onClick={props.onCancelChanges}/>
+          <AiOutlineSave className='save-button' onClick={saveChangesHandler}/>
+          <ImCancelCircle className='cancel-button' onClick={cancelChangesHandler}/>
        </div>
       )
   };
@@ -17,34 +75,33 @@ const card = props => {
   const renderReadMode = () => {
     return (
         <div className='header-default-buttons'>
-          <BiEdit className='edit-button' onClick={props.openEditMode}/>
-          <input type='checkbox' onChange={props.onChange} />
+          <BiEdit className='edit-button' onClick={openEditModeHandler}/>
+          <input type='checkbox' onChange={changeStyleHandler} />
         </div>
       )
   };
 
   return (
-    <div className={props.checked ? 'card active-status' : 'card'}>
-        <div className={props.checked ? 'card-header active' : 'card-header'}>
+    <div className={cardState.checked ? 'card active-status' : 'card'}>
+        <div className={cardState.checked ? 'card-header active' : 'card-header'}>
           <input type="text"
             className='card-header-text' 
-            readOnly={!props.isEditable}
-            value={props.headerText}
-            onChange={props.headerTextChangeHandler}
+            readOnly={!cardState.isEditable}
+            value={cardState.tempData.headerData}
+            onChange={event => cardDataChangeHandler(event, 'headerData')}
           />
-          <div className='header-buttons' >
-            {props.isEditable ? renderEditMode() : renderReadMode()}
+          <div className='header-buttons'>
+            {cardState.isEditable ? renderEditMode() : renderReadMode()}
           </div>
         </div>
         <textarea 
-          className={props.checked ? 'card-body active' : 'card-body'}
-          value={props.bodyText}
-          readOnly={!props.isEditable}
-          onChange={props.bodyTextChangeHandler}
-        >
-        </textarea>
+          className={cardState.checked ? 'card-body active' : 'card-body'}
+          value={cardState.tempData.bodyData}
+          readOnly={!cardState.isEditable}
+          onChange={event => cardDataChangeHandler(event, 'bodyData')}
+        />
     </div>
   )
 }
 
-export default card;
+export default Card;
