8a056d3042fa6500d14eb336c88a73c6344ab7a8
Valik Shlandyk <VShlandyk@gomel.iba.by>
2021-02-12 19:19:13 +0300

    Diving deeper - task9

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Card from '../CardList/Card';
+import { CardContext } from '../../context/card-context';
 
-const Cards = props => props.cards.map(card => {
-    return <Card
-              key={card.id}
-              cardData={card}
-              viewMode={props.viewMode}
-              onSave={props.onSave}
-              onChange={props.onChange}
-            />
-});
+const Cards = props => {
+    const {cards} = useContext(CardContext);
+    
+    return cards.map(card => {
+      return <Card
+                key={card.id}
+                cardData={card}
+                viewMode={props.viewMode}
+              />
+    });
+}
 
 export default Cards;
