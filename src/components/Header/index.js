c45a45c01a82c585077e1cba2fb0a7409a1f17ff
Valik Shlandyk <VShlandyk@gomel.iba.by>
2021-02-19 21:21:58 +0300

    Routing - task11

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,17 @@
 import React, { useContext } from 'react';
 import './style.css';
 import { CardContext } from '../../context/card-context';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     const {counter} = useContext(CardContext);
     return (
         <header>
             <h1 className='header_text'>Header</h1>
+            <nav className='links'>
+              <Link to='/' className='home'>Home</Link>
+              <Link to='/sign-in' className='sign_in'>Sign in</Link>
+            </nav>
             <div className='counter_box'>
                 <span className='counter_text'>Cards: </span>
                 <span className='counter'>{counter()}</span>
